ea5474466c917bbbac4eb5fbacf7a3499119f8e1
Jacob Smith <jakesmith.seo@gmail.com>
2022-07-26 04:32:48 -0500

    added more javascript

--- js/app.js
+++ js/app.js
@@ -13,8 +13,9 @@ const weeklyHours = document.querySelector('.weekly-hours');
 const biWeeklyHours = document.querySelector('.bi-weekly-hours');
 const monthlyHours = document.querySelector('.monthly-hours');
 
-// Select & Inputs
-const squareFootageSelect = document.getElementById('square-footage');
+// Forms & Inputs
+const formControl = document.querySelector('.form-control');
+const sqFootage = document.getElementById('square-footage');
 
 // DATA //
 const allDeepHours = [
@@ -23,23 +24,45 @@ const allDeepHours = [
 const allGeneralHours = [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5];
 
 // FUNCTIONS //
-const setCorrectData = (elementPrice, elementHours, arrElement, amount) => {
-  elementPrice.textContent = arrElement * amount;
+
+const setCorrectData = (
+  elementPrice,
+  elementHours,
+  arrElement,
+  amount,
+  taxRate
+) => {
+  const tax = (taxRate / 100) * (arrElement * amount);
+  elementPrice.textContent = arrElement * amount + tax;
   elementHours.textContent = arrElement;
 };
 
+const runData = (num, hourAmount, taxAmount) => {
+  setCorrectData(
+    deepPrice,
+    deepHours,
+    allDeepHours[num],
+    hourAmount,
+    taxAmount
+  );
+  setCorrectData(
+    generalPrice,
+    generalHours,
+    allGeneralHours[num],
+    hourAmount,
+    taxAmount
+  );
+};
+
 // EVENT HANDLERS //
-squareFootageSelect.addEventListener('change', e => {
+formControl.addEventListener('change', e => {
   const amountPerHour = +document.getElementById('amount-per-hour').value;
-  if (squareFootageSelect.value === '1000-1500') {
-    // deepPrice.textContent = allDeepHours[0] * amountPerHour;
-    // deepHours.textContent = allDeepHours[0];
-    setCorrectData(deepPrice, deepHours, allDeepHours[0], amountPerHour);
-    setCorrectData(
-      generalPrice,
-      generalHours,
-      allGeneralHours[0],
-      amountPerHour
-    );
+  const taxRate = +document.getElementById('tax-rate').value;
+  console.log(taxRate);
+  if (sqFootage.value === '1000-1500') {
+    runData(0, amountPerHour, taxRate);
+  }
+  if (sqFootage.value === '1600-2000') {
+    runData(1, amountPerHour, taxRate);
   }
 });
